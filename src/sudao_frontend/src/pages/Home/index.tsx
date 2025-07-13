25ba1addb3f274ce4e49c77e977798a46fa1f8d6
Kenneth Ezekiel <88850771+KenEzekiel@users.noreply.github.com>
2025-07-13 12:33:27 +0700

    add (payment): wallet connection

--- src/sudao_frontend/src/pages/Home/index.tsx
+++ src/sudao_frontend/src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useAccounts } from "@nfid/identitykit/react";
 
 const Home: React.FC = () => {
-    return (
-        <main className="flex flex-col w-full min-h-screen">
-            HALO INI HOME PAGE
-        </main>
-    );
+  const account = useAccounts();
+  console.log(account);
+  return (
+    <main className="flex flex-col w-full min-h-screen">
+      {account && (
+        <div>
+          {account.length > 0 && (
+            <div>
+              {account.map((account) => (
+                <div key={account.principal.toString()}>{account.principal.toString()}</div>
+              ))}
+            </div>
+          )}
+          <p>HALO INI HOME PAGE</p>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Home;
