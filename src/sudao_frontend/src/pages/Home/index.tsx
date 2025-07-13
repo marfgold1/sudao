3b9569eaeb2650282426b68c7d350657ed99c97d
Amar Fadil <amar.fadil@shopee.com>
2025-07-13 16:31:11 +0700

    fix: broken be import, add readme and fe integration example

--- src/sudao_frontend/src/pages/Home/index.tsx
+++ src/sudao_frontend/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Greet from "./greet";
 
 const Home: React.FC = () => {
     return (
         <main className="flex flex-col w-full min-h-screen">
             HALO INI HOME PAGE
+            <Greet />
         </main>
     );
 };
--- src/sudao_frontend/vite.config.ts
+++ src/sudao_frontend/vite.config.ts
@@ -9,6 +9,16 @@ dotenv.config({ path: '../../.env' });
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
+  },
   plugins: [
     react(),
     environment("all", { prefix: "CANISTER_" }),
